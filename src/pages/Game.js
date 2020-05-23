c98abe261ae1fdf47a2525b0a55dd6220f233c0b
Julian Büche <julian.bueche@student.hs-rm.de>
2020-05-23 13:00:39 +0200

    Habe der Gamepage mal 2 Testbuttons eingefügt und mal ein erstes Gefühl fürs Klicken zu bekommen. Generator erhört Clicks um 10. "Normaler" Click Button erhört um 1.

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import Button from "../components/Button";
 
 const Game = () => {
+    const [count, setCount] = useState(0)
+
+    const GeneratorOne = (event) => {
+        event.preventDefault()
+        setCount(count + 10)
+    }
+
     return(
-        <h1>HALLO!!</h1>
+        <div>
+            <h1>Coronoa-Inc Clicker Game</h1>
+            <Button onClick={() => setCount(count + 1)}>Click</Button>
+            <p>Clicks: {count}</p>
+            <h4>Generatoren</h4>
+            <Button onClick={GeneratorOne} >Level 1</Button>
+        </div>
     );
 }
 
