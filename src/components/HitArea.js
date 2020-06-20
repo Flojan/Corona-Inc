87dc5d77c0a3c703f0330eb6463ce49c19621983
fschm002 <florian@krygier-schmidt.de>
2020-06-20 12:00:10 +0200

    WebSocket Verbindung funktioniert nun einwandfrei nach dem Login; Mausklicks an den Server schicken funktioniert nun direkt

--- src/components/HitArea.js
+++ src/components/HitArea.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import HitButton from "../components/HitButton";
 import { useStoreState } from "easy-peasy";
@@ -7,23 +7,48 @@ export const StyledDiv = styled.div`
   margin: 40px 0 40px 0px;
 `;
 
-//öffnet Websocket Verbindung
-// let sendGeneratorClicksWS = new WebSocket(
-//   `ws://server.bykovski.de:8000/game/generators?token=${token}`
-// );
-// let getClicksWS = new WebSocket(
-//   `ws://server.bykovski.de:8000/game/balance?token=${token}`
-// );
+// // let token = sessionStorage.getItem("token");
+// // console.log(token);
+// // useeffect ->  Websocket state setzen + Token
+// //öffnet Websocket Verbindung
+// // let sendClickWS = new WebSocket(
+// //   `ws://server.bykovski.de:8000/game/click?token=${token}`
+// // );
+// // let sendGeneratorClicksWS = new WebSocket(
+// //   `ws://server.bykovski.de:8000/game/generators?token=${token}`
+// // );
+// // let getClicksWS = new WebSocket(
+// //   `ws://server.bykovski.de:8000/game/balance?token=${token}`
+// // );
 
 const HitArea = () => {
   const [count, setCount] = useState(0);
+  const [clickSocket, setClickSocket] = useState();
+  const [genClickSocket, setGenClickSocket] = useState();
+  const [getClickSocket, setGetClickSocket] = useState();
+
   const click = 1;
-  const token = useStoreState(state => state.user.token);
+  // token wird aus globalen State geholt
+  const token = useStoreState((state) => state.user.token);
   console.log("TOKEN", token);
-    
+
+  // wird erst ausgeführt wenn das Rendern der Komponente abgeschlossen ist
+  useEffect(() => {
+    const mouseClickWS = new WebSocket(
+      `ws://server.bykovski.de:8000/game/click?token=${token}`
+    );
+    // Socket wird in State abgespeichert
+    setClickSocket(mouseClickWS);
+    return () => {
+      mouseClickWS.close();
+    };
+  }, []);
+
+  console.log(clickSocket);
+
   //Methode um den Hit Click zu handeln und schickt click an den WebSocket
   const onHitClick = async (event) => {
-    //sendClickWS.send("click");
+    clickSocket.send("click");
     console.log("HitButton geklickt");
     setCount(count + click);
   };
@@ -37,3 +62,10 @@ const HitArea = () => {
 };
 
 export default HitArea;
+
+// /* kann man öfters kaufen // Jeder Generator kann öfters gekauft werden */
+// /* und jeder einzelne erhöht seinen preis, diesen bekommt er vom Server */
+// /* /generators​/{generator_id}​/buy  */
+// /* ​/generators​/{generator_id}​/next-price  */
+// // aktuelle Balance in GlobalState
+// // Upgrades kann man nur einmal kaufen // erhöht manuelle Click Zahlen
